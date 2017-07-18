f1146e7b6b73a686c1554da7a301977b0720b02a
GiselleSerate <giselle.serate@gmail.com>
2017-07-18 15:16:45 -0700

    Tack on an inventory module (which is functionally empty at the moment).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// These imports are all for Firebase.---------
 import { Observable } from 'rxjs/Observable';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
@@ -7,6 +8,9 @@ import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import * as firebase from 'firebase/app';
 import { Subject } from 'rxjs/Subject';
+// --------------------------------------------
+
+import { InventoryComponent } from './inventory/inventory.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// These imports are all for Firebase.----------
 import { Observable } from 'rxjs/Observable';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import * as firebase from 'firebase/app';
+//----------------------------------------------
+
+import { InventoryComponent } from './inventory/inventory.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
