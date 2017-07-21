e0658d38642302b628edf6e68d385f5fdb4c8b7c
GiselleSerate <giselle.serate@gmail.com>
2017-07-21 11:37:37 -0700

    Implement iou view (but you can't pay back yet).

--- src/app/ious/ious.component.ts
+++ src/app/ious/ious.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+// These imports are all for Firebase.---------
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { environment } from '../../environments/environment';
+import * as firebase from 'firebase/app';
+import { Subject } from 'rxjs/Subject';
+// --------------------------------------------
+
+import { User } from '../user';
 
 @Component({
   selector: 'app-ious',
@@ -7,7 +19,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IousComponent implements OnInit {
 
-  constructor() { }
+  db: AngularFireDatabase; // A reference to the database. 
+
+  people: [User]; // Local array of people so that this component can access all debtors/debtees.  
+  
+  currentUser: Observable<firebase.User>;  // An observable for the user that is logged in. 
+  currentUserData: Observable<any[]>;      // An observable for the people list. 
+
+  path: string; // Stores the database reference to the debts part of the tree. 
+
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.db = db;
+    this.currentUser = afAuth.authState;
+
+    this.currentUser.subscribe(res => {   // This callback block happens upon login or logout. 
+      if(res && res.uid) { // User logged in.
+
+        this.path ='/users/'.concat(res.uid, '/debts');
+
+        this.currentUserData = db.list(this.path); // Attach observable to the correct path. 
+        this.currentUserData.subscribe(snapshots => { // Begin observable's subscription. 
+		      this.people = [] as [User]; // Clear inventory array. 
+		      snapshots.forEach(snapshot => { // Iterate over snapshots and initialize items. 
+		        var item = new User(res.uid, snapshot.$key, snapshot.$value, this.db);
+		        item.init();
+		        this.people.push(item); // Put the items into the local array. 
+		      });
+		    })
+      }
+      else { // User not logged in.
+        // Unassign all variables storing information about the user. 
+        this.people = null;
+        this.currentUserData = null;
+      }
+    })
+  }
 
   ngOnInit() {
   }
---
+++ src/app/user.ts
@@ -0,0 +1,51 @@
+// These imports are all for Firebase.---------
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import * as firebase from 'firebase/app';
+import { Subject } from 'rxjs/Subject';
+// --------------------------------------------
+
+export class User {
+  currentUserID: string; // The current user's id. 
+  userID: string;
+  debt: number;
+  db: AngularFireDatabase;
+  name: string;
+  handle: string;
+
+  constructor(currentUserID: string, userID: string, debt: number, db: AngularFireDatabase) { // If you don't need the debt, set it to 0. It shouldn't mess anything up. 
+    this.currentUserID = currentUserID;
+    this.userID = userID;
+    this.debt = debt;
+    this.db = db;
+    // From the userID, I can calculate the other properties of the user in question. For now, I will initialize them to an default string so it fails semi-gracefully. 
+    this.name = "waiting";
+    this.handle = "waiting";
+  }
+
+  init() { // Initialize the user immediately following construction. Remember to call me! 
+    this.db.object('/users/'.concat(this.userID)).subscribe(snapshot => { // Begin observable's subscription. 
+      // Set the user's properties. 
+      this.name = snapshot.name;
+      this.handle = snapshot.handle;
+    })
+  }
+
+  save() {
+    console.log("Saving debt with: ".concat(this.name,", uid: ",this.userID,", amount: ",this.debt.toString()));
+
+    var myKey = this.userID as string; // For some reason putting this.userID doesn't really work in the .update function. It is angery. 
+
+    // Send update to the database. 
+    this.db.object('/users/'.concat(this.currentUserID,'/debts')).update({
+      myKey: this.debt
+    });
+  }
+
+  addDebt(amount: number) { // A debt amount to add to the debts.
+
+  }
+
+}
